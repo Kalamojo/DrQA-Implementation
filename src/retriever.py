9748d0e74a4ff63ac7be194a868c3cd4bdd034cb
Kalamojo <kojo22444488888888@gmail.com>
2024-05-05 22:35:05 -0500

    TF-IDF Demo

--- src/retriever.py
+++ src/retriever.py
@@ -1,8 +1,29 @@
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import spmatrix
+from nltk import word_tokenize
+import nltk
+nltk.download('punkt')
+import joblib
 
 class DocumentRetriever:
-    def __init__(self):
-        pass
+    def __init__(self, bigram: bool = True, vectorizer_path: str = None) -> None:
+        self.vectorizer = self.load_vectorizer(vectorizer_path, bigram)
+
+    def fit_documents(self, documents: list[str]) -> None:
+        self.vectorizer.fit(documents)
+
+    def get_vectors(self, documents: list[str]) -> spmatrix:
+        return self.vectorizer.transform(documents)
+
+    def tokenizer(self, text: str) -> list[str]:
+        return word_tokenize(text)
     
-    def howdy(self):
-        print("Howdy, pardner")
+    def load_vectorizer(self, path: str = None, bigram: bool = True) -> TfidfVectorizer:
+        if path:
+            return joblib.load(path)
+        else:
+            ngram = (1,2) if bigram else (1,1)
+            return TfidfVectorizer(ngram_range=ngram, lowercase=True, strip_accents='unicode', tokenizer=self.tokenizer)
+    
+    def save_vectorizer(self, path: str) -> None:
+        joblib.dump(self.vectorizer, path, compress=True)
--- src/tester.py
+++ src/tester.py
@@ -1,8 +1,15 @@
 from retriever import DocumentRetriever
 
 def main():
-    doc_ret = DocumentRetriever()
-    doc_ret.howdy()
+    doc_ret = DocumentRetriever(bigram=False)
+    docs = ["I like dogs",
+            "Dogs like dogs",
+            "I like cats"]
+    
+    doc_ret.fit_documents(documents=docs)
+    matrix = doc_ret.get_vectors(documents=docs)
+    print(doc_ret.vectorizer.get_feature_names_out())
+    print(matrix.toarray())
 
 if __name__ == '__main__':
     main()
