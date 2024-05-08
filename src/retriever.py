7c9a3c7b4a87eb39c020539e95618440c337011e
Kalamojo <kojo22444488888888@gmail.com>
2024-05-08 00:25:10 -0500

    Resolved pickling error

--- src/retriever.py
+++ src/retriever.py
@@ -5,35 +5,7 @@ import nltk
 nltk.download('punkt')
 import joblib
 
-class Vectorizer1(object):
-    def __init__(self, bigram: bool = True, vectorizer_path: str = None) -> None:
-        self.vectorizer = self.__load_vectorizer(vectorizer_path, bigram)
-
-    def fit_documents(self, documents: list[str]) -> None:
-        self.vectorizer.fit(documents)
-
-    def get_vectors(self, documents: list[str]) -> spmatrix:
-        return self.vectorizer.transform(documents)
-
-    def __tokenizer(self, text: str) -> list[str]:
-        return word_tokenize(text)
-    
-    def __load_vectorizer(self, path: str = None, bigram: bool = True) -> TfidfVectorizer:
-        if path:
-            return joblib.load(path)
-        else:
-            ngram = (1,2) if bigram else (1,1)
-            return TfidfVectorizer(ngram_range=ngram, lowercase=True, strip_accents='unicode', tokenizer=self.__tokenizer)
-    
-    def save_vectorizer(self, path: str) -> None:
-        joblib.dump(self.vectorizer, path, compress=True)
-
 class Vectorizer(TfidfVectorizer):
-    def __new__(cls, *args, **kw):
-        f = super().__new__(cls)
-        f.__init__(*args, **kw)
-        return f
-    
     def __init__(self, bigram: bool = True, vectorizer_path: str = None) -> None:
         self.bigram = bigram
         self.vectorizer_path = vectorizer_path
@@ -41,15 +13,25 @@ class Vectorizer(TfidfVectorizer):
 
     def __tokenizer(self, text: str) -> list[str]:
         return word_tokenize(text)
-
+    
+    def save_vectorizer(self, path: str) -> None:
+        joblib.dump(self.__getstate__(), path, compress=True)
+    
     def __load_vectorizer(self) -> None:
         if self.vectorizer_path:
-            thing = joblib.load(self.vectorizer_path)
-            print(thing)
-            self.__dict__ = thing
+            self.__setstate__(joblib.load(self.vectorizer_path))
         else:
             ngram = (1,2) if self.bigram else (1,1)
             super().__init__(ngram_range=ngram, lowercase=True, strip_accents='unicode', tokenizer=self.__tokenizer)
-    
-    def save_vectorizer(self, path: str) -> None:
-        joblib.dump(self.__dict__, path, compress=True)
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        # Remove the unpicklable entries.
+        del state["tokenizer"]
+        return state
+
+    def __setstate__(self, state):
+        # Restore instance attributes
+        self.__dict__.update(state)
+        self.tokenizer = self.__tokenizer
+
--- src/tester.py
+++ src/tester.py
@@ -1,17 +1,17 @@
 from retriever import Vectorizer
 
 def main():
-    vectorizer_path = "vectorizer.pkl"
+    vectorizer_path = None
     doc_ret = Vectorizer(bigram=False, vectorizer_path=vectorizer_path)
     docs = ["I like dogs",
             "Dogs like dogs",
             "I like cats"]
     
-    #doc_ret.fit(docs)
-    #doc_ret.save_vectorizer("vectorizer.pkl")
-    matrix = doc_ret.transform(docs)
-    print(doc_ret.get_feature_names_out())
-    print(matrix.toarray())
+    doc_ret.fit(docs)
+    doc_ret.save_vectorizer("vectorizer.pkl")
+    # matrix = doc_ret.transform(docs)
+    # print(doc_ret.get_feature_names_out())
+    # print(matrix.toarray())
 
 if __name__ == '__main__':
     main()
