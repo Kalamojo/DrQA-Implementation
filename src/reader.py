bea85f1752a2c281e26b3129367027902e6cbf5c
Kalamojo <kojo22444488888888@gmail.com>
2024-06-21 21:47:16 -0500

    Exact match logic

--- src/reader.py
+++ src/reader.py
@@ -3,6 +3,7 @@ import h5py
 import numpy as np
 from mittens import Mittens, GloVe
 from sklearn.feature_extraction.text import CountVectorizer
+from nltk.tokenize import word_tokenize
 from collections import Counter
 import warnings
 import time
@@ -13,13 +14,12 @@ class Embedder(object):
         self.embed_path = embed_path
         self.glove_path = glove_path
         self.vocab, self.embeddings = self.__load_embedder()
-        self.dimensions = self.embeddings.shape[1]
+        self.dimensions = self.embeddings[0].shape[0]
         print(self.dimensions)
     
     def embed(self, word: str) -> np.ndarray:
-        if word not in self.model:
-            raise KeyError("Word has not been seen by the embedding model")
-        return self.model[word]
+        ind = self.vocab.index(word)
+        return self.embeddings[ind]
 
     def fine_tune(self, words: list[str], documents: list[str], max_iterations: int = 1000, vocab_save: str = None, embed_save: str = None) -> None:
         # new_words = [token for token in words if token not in self.model.keys()]
@@ -33,14 +33,17 @@ class Embedder(object):
         coocc_matrix = Xc.toarray()
         print(coocc_matrix.shape)
 
+        pre_trained = dict(zip(self.vocab, self.embeddings))
         mittens_model = Mittens(n=self.dimensions, max_iter=max_iterations)
         new_embeddings = mittens_model.fit(
             coocc_matrix,
             vocab=corp_vocab,
-            initial_embedding_dict=self.model)
+            initial_embedding_dict=pre_trained)
 
         for i in range(len(corp_vocab)):
-            self.model[corp_vocab[i]] = new_embeddings[i, :]
+            pre_trained[corp_vocab[i]] = new_embeddings[i, :]
+
+        self.vocab, self.embeddings = zip(*pre_trained.items())
 
         if vocab_save is not None and embed_save is not None:
             self.save_embedder(vocab_save, embed_save)
@@ -48,12 +51,11 @@ class Embedder(object):
             warnings.warn("Vectorizer and Matrix were not saved because paths were not supplied")
     
     def save_embedder(self, vocab_path: str, embed_path: str) -> None:
-        #word_list, vectors = zip(*self.model.items())
         with open(vocab_path, 'w', encoding='utf-8') as f:
             f.writelines([line + '\n' for line in self.vocab])
         np.save(embed_path, self.embeddings)
 
-    def __load_embedder(self) -> tuple[list[str], np.ndarray]:
+    def __load_embedder(self) -> tuple[list[str], list[np.ndarray]]:
         if self.vocab_path is not None and self.embed_path is not None:
             return self.__load_saved_embedder(self.vocab_path, self.embed_path)
         elif self.glove_path is not None:
@@ -61,7 +63,7 @@ class Embedder(object):
         
         raise ValueError("Either vocab and embed paths are required to load saved Embedder,\n or glove_path is required to create a new one")
     
-    def __glove2dict(self, path) -> tuple[list[str], np.ndarray]:
+    def __glove2dict(self, path) -> tuple[list[str], list[np.ndarray]]:
         with open(path, 'r', encoding='utf-8') as f:
             reader = csv.reader(f, delimiter=' ', quoting=csv.QUOTE_NONE)
             vocab = []
@@ -69,9 +71,9 @@ class Embedder(object):
             for line in reader:
                 vocab.append(line[0])
                 embeddings.append(np.array(list(map(float, line[1:]))))
-        return vocab, np.array(embeddings)
+        return vocab, embeddings
     
-    def __load_saved_embedder(self, vocab_path: str, embed_path: str) -> tuple[list[str], np.ndarray]:
+    def __load_saved_embedder(self, vocab_path: str, embed_path: str) -> tuple[list[str], list[np.ndarray]]:
         with open(vocab_path, 'r', encoding='utf-8') as f:
             vocab = [line.strip() for line in f]
         embeddings = np.load(embed_path, mmap_mode='r')
@@ -80,6 +82,7 @@ class Embedder(object):
 class Reader(object):
     def __init__(self, vocab_path: str = None, embed_path: str = None, glove_path: str = None) -> None:
         self.embedder = Embedder(vocab_path, embed_path, glove_path)
+        self.lemmatizer = WordNetLemmatizer()
 
     def fine_tune_embedder(self, documents: list[str], common_count: int = 1000, vocab_save: str = None, embed_save: str = None) -> None:
         all_words = ' '.join(documents).split()
@@ -87,4 +90,13 @@ class Reader(object):
         common_words, counts = zip(counter.most_common(common_count))
         self.embedder.fine_tune(common_words, documents, vocab_save, embed_save)
 
-    
+    def __match_tokenize(self, sentence: str) -> tuple(list[str], list[str], list[str]):
+        sentence_og = word_tokenize(sentence)
+        sentence_lower = [word.lower() for word in sentence_og]
+        sentence_lemma = [self.lemmatizer.lemmatize()]
+
+    def __exact_match(self, word: str, question_og: list[str], question_lower: list[str], question_lemma: list[str]) -> np.ndarray:
+        og = int(word in question_og)
+        low = int(word.lower() in question_lower)
+        lem = int(self.lemmatizer.lemmatize(word) in question_lemma)
+        return np.array([og, low, lem])
